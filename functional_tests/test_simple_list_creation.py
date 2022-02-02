8803ed7d0e7ac5fa6c6d18fd118b6fc3bb1e0ead
Higor Andrade <higor.andrade@concert.com.br>
2022-02-02 19:20:32 -0300

    Refatorando o Teste Funcional

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,18 +1,9 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
 import time
 
-MAX_WAIT = 5
-
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-        self.browser.quit()
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_for_one_user(self): 
     
@@ -34,7 +25,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # Quando ela aperta enter, a página atualiza, e mostra a lista
         # "1: Estudar testes funcionais" como um item da lista TODO
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
+        FunctionalTest.wait_for_row_in_list_table(self, '1: Estudar testes funcionais')
         
         # Ainda existe uma caixa de texto convidando para adicionar outro item
         # Ela digita: "Estudar testes de unidade"
@@ -43,8 +34,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # A página atualiza novamente, e agora mostra ambos os itens na sua lista
-        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
-        self.wait_for_row_in_list_table('2: Estudar testes de unidade')
+        FunctionalTest.wait_for_row_in_list_table(self, '1: Estudar testes funcionais')
+        FunctionalTest.wait_for_row_in_list_table(self, '2: Estudar testes de unidade')
         
         # Ainda existe uma caixa de texto convidando para adicionar outro item
         # Ela digita: "Estudar testes de unidade"
@@ -72,7 +63,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Estudar testes funcionais')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
+        FunctionalTest.wait_for_row_in_list_table(self, '1: Estudar testes funcionais')
 
         # Ela nota que sua lista possui uma URL única
         maria_list_url = self.browser.current_url
@@ -92,7 +83,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Comprar leite')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Comprar leite')
+        FunctionalTest.wait_for_row_in_list_table(self, '1: Comprar leite')
 
         # João pega sua URL única
         joao_list_url = self.browser.current_url
@@ -105,41 +96,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Comprar leite', page_text)
 
         # Satisfeitos, ambos vão dormir
-
-    def test_layout_and_styling(self):
-        # Edith entra na home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # Ela nota que o input box está centralizado
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-        # Ela inicia uma nova lista e nota que o input
-        # também está centralizado
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:  
-            try:
-                table = self.browser.find_element_by_id('id_list_table')  
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return  
-            except (AssertionError, WebDriverException) as e:  
-                if time.time() - start_time > MAX_WAIT:  
-                    raise e  
-                time.sleep(0.5)
